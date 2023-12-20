9fe6be316dddf1aecce41c66ad09bec3fd6be010
Georgiy <georgmarts@gmail.com>
2023-12-20 23:05:34 +0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 window.onload = function () {
-  const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".swiper-1", {
     direction: "horizontal",
     loop: true,
 
-    // pagination: {
-    //   el: ".swiper-pagination",
-    // },
-
     navigation: {
-      nextEl: ".slider1__button-next",
-      prevEl: ".slider1__button-prev",
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
     },
 
     scrollbar: {
       el: ".swiper-scrollbar",
     },
+  });
+
+  const swiper2 = new Swiper(".swiper-2", {
+    direction: "horizontal",
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  const swiper3 = new Swiper(".swiper-3", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: "5",
+    spaceBetween: "200",
   });
 };
